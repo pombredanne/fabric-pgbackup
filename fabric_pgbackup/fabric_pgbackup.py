18fd1d43f193a2f2ac042069316317669020fcff
Jakub Zalewski <zalew7@gmail.com>
2013-09-19 20:27:34 +0200

    api and config mockup

---
+++ fabric_pgbackup/fabric_pgbackup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def pg_list_backups():
+    """
+
+    """
+    pass
+
+
+def pg_backup():
+    """
+
+    """
+    pass
+
+
+def pg_download():
+    """
+
+    """
+    pass
+
+
+def pg_restore():
+    """
+
+    """
+    pass
+
+
+def pg_check():
+    """
+
+    """
+    pass
